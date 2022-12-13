7a9ced649dbdecc0c207e8894535f647cc4d8681
urbain32 <urbainq32@gmail.com>
2022-12-13 18:04:31 +0200

    create a load data to get data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,77 @@
 import { Delete } from "@mui/icons-material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Create from "./Create";
 // Styles
 import "./Home.css";
 export default function Home() {
-  const { data, isPending, error } = useFetch(
-    " http://localhost:3002/articles"
-  );
+  // const { data, isPending, error } = useFetch(
+  //   " http://localhost:3002/articles"
+  // );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("Ajouter");
+  const [currentBank, setCurrentBank] = useState({
+    title: "",
+    author: "",
+    body: "",
+  });
+  const setCurrent = (event, row) => {
+    setCurrentBank({
+      id: row.id,
+      title: row.title,
+      author: row.author,
+      body: row.body,
+    });
+    setTitle("Modifier");
+  };
+  const url = "http://localhost:3002/articles";
+  const loadData = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((res) => {
+        setData(res.data);
+        // console.log('data', res.data);
+      })
+      .catch((err) => {
+        console.log("error message", err.message);
+        setLoading(false);
+      });
+    setCurrentBank({
+      title: "",
+      author: "",
+      body: "",
+    });
+    setTitle("Ajouter");
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+
   const handleDelete = (id) => {
     axios.delete(`http://localhost:3002/articles/${id}`).then(() => {
       console.log("Data delete");
+      loadData();
     });
   };
   return (
     <div className="home">
       <h2>Articles</h2>
       <div className="create">
-        <Create />
+        <Create
+          data={data}
+          handleDelete={handleDelete}
+          title={title}
+          setTitle={setTitle}
+          currentBank={currentBank}
+          loadData={loadData}
+        />
       </div>
-      {error && <div> {error}</div>}
-      {isPending && <div> Loading...</div>}
+      {/* {error && <div> {error}</div>}
+      {isPending && <div> Loading...</div>} */}
       {data &&
         data.map((article) => (
           <div className="card" key={article.id}>
